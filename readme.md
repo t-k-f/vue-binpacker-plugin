c1e640601e7013d24fa26985895756e0341b535e
Jan Thoma <jan.thoma@t-k-f.ch>
2019-05-22 10:35:30 +0200

    init commit

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Vue.js Packery Plugin
+
+A Bin Packer Plugin. Similiar to the well known Packery based on [Nikolay Karev's work](https://github.com/karevn/binpack-2d).
+
+### Installing
+
+```
+npm install --save vue-binpacker-plugin
+```
+
+then
+
+```
+import VueBinpackerPlugin from 'vue-binpacker'
+
+Vue.use(VueBinpackerPlugin)
+```
+
+### Usage
+
+```
+<VueBinpacker>
+
+
+    <div data-packer-item='true'></div>
+    <div data-packer-item='true'></div>
+
+<VueBinpacker>
+```
+
+### Roadmap
+
+- Testing for Browser Compatibility
+- Extend documentation
+- Support for horizontal layouts
+- Eventhandling for manually triggering layouts
---
+++ src/index.js
@@ -0,0 +1,90 @@
+import Vue from 'vue'
+import packer from '../lib/packer.js'
+
+export default {
+
+    install (Vue, options)
+    {
+        Vue.component('vueBinpacker', {
+            render: function (createElement)
+            {
+                return createElement(
+                    'div',
+                    this.$slots.default
+                )
+            },
+            data ()
+            {
+                return {
+                    observer: null,
+                    observerPause: false
+                }
+            },
+            created ()
+            {
+                window.addEventListener('resize', this.setPacker)
+            },
+            beforeDestroy ()
+            {
+                window.removeEventListener('resize', this.setPacker)
+            },
+            mounted ()
+            {
+                this.setObserver()
+                this.setPacker()
+                this.$el.style.position = 'relative'
+            },
+            methods:
+            {
+                setObserver ()
+                {
+                    this.observer = new MutationObserver(this.setObserverMutations)
+                    this.observer.observe(this.$el, { childList: true, attributes: true, subtree: true })
+                },
+                setObserverMutations (mutations)
+                {
+                    if (this.observerPause)
+                    {
+                        return
+                    }
+
+                    this.setPacker()
+                },
+                setPacker ()
+                {
+                    this.observerPause = true
+
+                    const width = this.$el.getBoundingClientRect().width
+                    const nodes = this.$el.querySelectorAll('[data-packer-item="true"]')
+                    const rects = []
+
+                    for (let i = 0; i < nodes.length; i++)
+                    {
+                        let rect = nodes[i].getBoundingClientRect()
+
+                        rects.push({
+                            width: Math.floor(rect.width),
+                            height: Math.floor(rect.height)
+                        })
+                    }
+
+                    const container = { width: width, height: Infinity }
+                    const result = packer(container, rects)
+
+                    for (let i = 0; i < nodes.length; i++)
+                    {
+                        nodes[i].style.position = 'absolute'
+                        nodes[i].style.left = 0
+                        nodes[i].style.top = 0
+                        nodes[i].style.transform = 'translate(' + result[i].x + 'px, ' + result[i].y + 'px)'
+                    }
+
+                    this.$nextTick(() =>
+                    {
+                        this.observerPause = false
+                    })
+                }
+            }
+        })
+    }
+}
